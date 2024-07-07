dde5bc8cf2fbbd5d2caeba3811ac6398898eb454
Ganeshvedhachalam <ganeshgreat08@gmail.com>
2024-07-07 12:34:17 +0530

    currying

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-let a=1;
-let b=3;
-console.log(a+b+5);
-console.log(a)
+// let a=1;
+// let b=3;
+// console.log(a+b+5);
+// console.log(a)
+
+/// learn js sfundamentals 
+// currying - ordering functions inside functions ton reduce the parameters arity
+function multiply(a){
+
+    return function (b){  
+        return a*b// uses closure of a 
+    }
+
+}
+
+const twosum = multiply(2)
+console.log(twosum(4))
+
+// by bind 
+function multi(a,b){
+    return a*b
+
+}
+let threemulti = multi.bind(this,3)
+console.log(threemulti(4))
